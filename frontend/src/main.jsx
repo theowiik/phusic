c76d53d53ff2a51edbdda3cad86be17a4b02bc5f
Theo Wiik <theo.le.wiik@gmail.com>
2025-11-02 13:02:53 +0100

    Start of config builder

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './app.jsx'
+import ConfigBuilder from './ConfigBuilder.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/config" element={<ConfigBuilder />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
